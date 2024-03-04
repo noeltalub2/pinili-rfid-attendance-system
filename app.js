a24e5c3bd33d80a32bd113105a487d4cfe4903d2
Noel Michael Talub <jmtalub152@gmail.com>
2024-03-04 23:01:02 +0800

    Fix now

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-const express = require('express');
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+	res.send("Hello");
+});
+
+app.listen(PORT, () => {
+	console.log(`The server is running at port ${PORT}`);
+});
